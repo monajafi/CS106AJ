e18e655db79b2f256be2e8678b7b3147348ee5bd
Mousa Najafi <monajafi@gmail.com>
2018-12-31 05:11:54 +0330

    Centered GLabel example completed

--- BookCodes/ch4/CenteredLabel/CenterLabel.js
+++ BookCodes/ch4/CenteredLabel/CenterLabel.js
@@ -7,8 +7,8 @@ const GWINDOW_HEIGHT = 200;
 function CenteredLabel(){
     let gw = GWindow(GWINDOW_WIDTH,GWINDOW_HEIGHT);
     let msg = GLabel("Hello World!");
-    msg.setFont("24px 'Times New Roman'");
-    gw.add(msg,10,10);
-    let rect = GRect(0,0,100,100);
-    gw.add(rect,10,10);
+    msg.setFont("36px 'Lucida Blackletter',Serif");
+    let x = (gw.getWidth() - msg.getWidth()) / 2;
+    let y = (gw.getHeight() + msg.getAscent()) / 2;
+    gw.add(msg,x,y);
 }
