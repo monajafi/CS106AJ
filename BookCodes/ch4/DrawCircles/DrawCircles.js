9f69b513f98338501cdb56bbc52e9d71f058d0d7
Mousa Najafi <monajafi@gmail.com>
2019-01-01 05:57:43 +0330

    DrawCircles added

---
+++ BookCodes/ch4/DrawCircles/DrawCircles.js
@@ -0,0 +1,25 @@
+/// <reference path="../libs/JSGraphics.js" />
+"use strict";
+
+/* constants */
+const GWINDOW_WIDTH = 500;
+const GWINDOW_HEIGHT = 300;
+const CIRCLE_COUNT = 5;
+const CIRCLE_SIZE = 80;
+const SPACING = 10;
+
+/*
+ * Draws five circles centered in graphical window gw
+ * x and y indicates the upper left corner of bounding box that 
+ * surrounds the five circles
+ */
+
+ function DrawCircles(){
+    let gw = GWindow(GWINDOW_WIDTH,GWINDOW_HEIGHT);
+    let x = (GWINDOW_WIDTH - (CIRCLE_COUNT *(CIRCLE_SIZE +SPACING -1))) / 2;
+    let y = (GWINDOW_HEIGHT - CIRCLE_SIZE) / 2;
+    for(let i = 0;i < CIRCLE_COUNT;i++){
+        gw.add(GOval(x,y,CIRCLE_SIZE,CIRCLE_SIZE));
+        x += CIRCLE_SIZE + SPACING;
+    }
+ }
