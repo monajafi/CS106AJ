61af755b6986788bfa4ef7293783a36056b6822b
Mousa Najafi <monajafi@gmail.com>
2018-12-31 23:32:05 -0800

    DrawCheckerboard added

---
+++ BookCodes/ch4/DrawCheckerboard/DrawCheckerboard.js
@@ -0,0 +1,27 @@
+/// <reference path="../libs/JSGraphics.js"/>
+"use strict";
+/* Checherboard constants */
+
+const GWINDOW_WIDTH = 600;
+const GWINDOW_HEIGHT = 500;
+const N_COLUMNS = 10;
+const N_ROWS = 10;
+const SQUARE_SIZE = 35;
+
+/* 
+ * Draws N_Colmn by N_Row checherboard in the center of graphical window gw
+ * x and y indicates the upper left corner of bounding box surrounding checkerboard
+ */
+
+ function DrawCheckerboard(){
+     let gw = GWindow(GWINDOW_WIDTH,GWINDOW_HEIGHT);
+     let x = (GWINDOW_WIDTH - (N_COLUMNS * SQUARE_SIZE)) / 2;
+     let y = (GWINDOW_HEIGHT - (N_ROWS * SQUARE_SIZE)) / 2;
+     for(let row=0;row < N_ROWS;row++){
+         for(let column = 0;column < N_COLUMNS;column++){
+             let square = GRect(x + column * SQUARE_SIZE,y + row * SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE);
+             square.setFilled((row + column) % 2 !== 0);
+             gw.add(square);
+         }
+     }
+ }
