ba83cf25eaa082ab044e79c89d79d4e053b324af
Mousa Najafi <monajafi@gmail.com>
2019-02-20 06:45:25 +0330

    Exercise 11

--- BookCodes/ch6/P11-Wudnt/Wudnt.js
+++ BookCodes/ch6/P11-Wudnt/Wudnt.js
@@ -21,18 +21,17 @@
     let gw = GWindow(GWINDOW_WIDTH,GWINDOW_HEIGHT);
     let cx = (gw.getWidth() - 2 * TOTAL_WIDTH) / 2;
     let cy = (gw.getHeight() - TOTAL_HEIGHT) / 2;  
-    // upper curve radius is equal to TOTAL_WIDTH according to the attached trigonemetric figure
-    let upper = drawGArc(cx,cy,TOTAL_WIDTH,true,"Orange");
-    upper.setColor("Transparent");
-    gw.add(upper);
-    let lower = drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT,TOTAL_WIDTH - WUDNT_HEIGHT,true,"White");
-    lower.setColor("White");
-    gw.add(lower);
-  //  gw.add(drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT,TOTAL_WIDTH - WUDNT_HEIGHT,true,"White"));
+    let upperRadius = TOTAL_WIDTH / (2 * Math.cos(WUDNT_ANGLE * Math.PI / 180));
+    gw.add(drawGArc(cx,cy,upperRadius,"Orange"));
+    gw.add(drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT,upperRadius - WUDNT_HEIGHT,"White"));
+    let circle = GOval(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT,2 * (upperRadius - WUDNT_HEIGHT),2* (upperRadius - WUDNT_HEIGHT));
+    circle.setFilled(true);
+    circle.setFillColor("White");
+    circle.setColor("Transparent");
+    gw.add(circle);
     let gap = WUDNT_HEIGHT + TOTAL_HEIGHT - 2 * ((TOTAL_WIDTH - WUDNT_HEIGHT) / Math.tan((90 + WUDNT_ANGLE) * Math.PI / (2 * 180)) + WUDNT_HEIGHT);
-    gw.add(drawGArc(cx,cy + gap,TOTAL_WIDTH,true,true,"Orange"));
-    gw.add(drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT + gap,TOTAL_WIDTH - WUDNT_HEIGHT,true,false,"White"));
-    gw.add(drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT + gap,TOTAL_WIDTH - WUDNT_HEIGHT,false,true,"White"));
+    gw.add(drawGArc(cx,cy + gap,upperRadius,"Orange"));
+    gw.add(drawGArc(cx + WUDNT_HEIGHT,cy + WUDNT_HEIGHT + gap,upperRadius - WUDNT_HEIGHT,"White"));
 }
 
 /* 
@@ -40,11 +39,9 @@
  * start angle to make curve symmetric.
  */
 
-function drawGArc(x,y,r,filled,color) {
+function drawGArc(x,y,r,color,borderColor) {
     let gArc = GArc(x,y,2 * r,2 * r,WUDNT_ANGLE,180 - 2 * WUDNT_ANGLE);
-    gArc.setFilled(filled);
-    if(filled){
-        gArc.setFillColor(color);
-    }
+    gArc.setFilled(true);
+    gArc.setFillColor(color);
     return gArc;
 }
